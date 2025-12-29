5221823b12d845c78eddef4f744725445f7869cf
Assefa Endale <76453266+Asecoding@users.noreply.github.com>
2025-12-29 20:18:46 +0300

    Change string representation to product name

--- Alx_ecommerce_project/ecommerce_app/models.py
+++ Alx_ecommerce_project/ecommerce_app/models.py
@@ -16,4 +16,4 @@ class Post(models.Model):
     marchant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
-        return self.title
+        return self.product
