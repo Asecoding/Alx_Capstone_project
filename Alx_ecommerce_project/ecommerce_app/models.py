52b19c7372e0f5baf234ee0eeeced682a056b026
Assefa Endale <76453266+Asecoding@users.noreply.github.com>
2025-12-29 20:18:06 +0300

    Create Post model for product representation
    Added Post model to represent product posts with fields for product name, category, post date, and merchant.

--- Alx_ecommerce_project/ecommerce_app/models.py
+++ Alx_ecommerce_project/ecommerce_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Post model represents each product post.
+    - product: name of the product
+    - catagory: catagory of product
+    - post_date: automatically records date/time when created
+    - marchant: foreign key referencing Django's built-in User model
+    """
+
+    productname = models.CharField(max_length=200)
+    catagory = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+    marchant = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+
+    def __str__(self):
+        return self.title
